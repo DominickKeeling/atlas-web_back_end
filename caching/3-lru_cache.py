4550797e9d84a41de41de8cb43bd9b8fca6a2bf6
dom <6157@holbertonstudents.com>
2024-01-10 23:21:27 -0600

    fixed task 4

--- caching/3-lru_cache.py
+++ caching/3-lru_cache.py
@@ -22,32 +22,40 @@ def get(self, key):
     If key is None or if the key doesnt exist in self.cache_data, return None.
 """
 
+#!/usr/bin/env python3
+
+""" Create a class LRUCache that inherits from BaseCaching
+    and is a caching system """
+
 BaseCaching = __import__('base_caching').BaseCaching
 
 
 class LRUCache(BaseCaching):
-    """ LRU cache class """
+    """ Create a class LRUCache that inherits from BaseCaching
+        and is a caching system """
+
     def __init__(self):
-        """ Initializing """
-        super.__init__()
-        self.key_order = []
+        """ Constructor """
+        super().__init__()
+        self.queue = []
 
     def put(self, key, item):
-        """ Appending item to the cache data """
-        if key is not None and item is not None:
-            if key in self.cach_data:
-                self.key_order.remove(key)
-            self.cache_data[key] = item
-            self.key_order.append(key)
-            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-                lru_key = self.key_order.pop(0)
-                del self.cache_data[lru_key]
-                print(f"DISCARD: {lru_key}\n")
+        """ Add an item in the cache """
+        if key is None or item is None:
+            return
+        if key in self.cache_data.keys():
+            self.queue.remove(key)
+        elif len(self.cache_data.keys()) >= self.MAX_ITEMS:
+            discard = self.queue.pop(0)
+            del self.cache_data[discard]
+            print('DISCARD: {}'.format(discard))
+        self.queue.append(key)
+        self.cache_data[key] = item
 
     def get(self, key):
-        """ Retrieving key item, removing it and adding it again """
-        if key is not None and key in self.cache_data:
-            self.discarded_key.remove(key)
-            self.discarded_key.append(key)
-            return self.cache_data[key]
-        return None
+        """ Get an item by key """
+        if key is None or key not in self.cache_data.keys():
+            return None
+        self.queue.remove(key)
+        self.queue.append(key)
+        return self.cache_data[key]
