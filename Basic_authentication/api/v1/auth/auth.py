ce5cc75bffff112f78723b0266734c32cbda4a96
dom <6157@holbertonstudents.com>
2024-01-17 13:10:47 -0600

    intial push from remote

---
+++ Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+""" Create a class to manage the API auth """
+
+from flask import request
+from typing import List, TypeVar
+
+class Auth():
+    """ auth class """
+    
+    def __init__(self):
+        """ init """
+        pass
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ require auth """
+        if path is None or excluded_paths is None or excluded_paths == []:
+            return True
+        if path[-1] != '/':
+            path += '/'
+        if path in excluded_paths:
+            return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """ define authorization_header """
+        if request is None:
+            return None
+        if 'Authorization' not in request.headers:
+            return None
+        return request.headers['Authorization']
+
+    def current_user(self) -> TypeVar('User'):
+        """ define current_user """
+        return None
+      
