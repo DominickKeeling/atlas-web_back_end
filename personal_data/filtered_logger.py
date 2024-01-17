0bcb34905ed691c528f4de8325f9fb17d162a337
dom <6157@holbertonstudents.com>
2024-01-17 11:10:45 -0600

    fixing pycode style

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -18,7 +18,7 @@ def filter_datum(fields: List[str], redaction: str, message: str,
     """ returns log message """
     for field in fields:
         message = re.sub(f'{field}=.*?{separator}',
-                        f'{field}={redaction}{separator}', message)
+                         f'{field}={redaction}{separator}', message)
     return message
 
 
@@ -61,9 +61,9 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
     db_host = os.getenv('PERSONAL_DATA_DB_HOST', 'localhost')
     db_name = os.getenv('PERSONAL_DATA_DB_NAME')
     return mysql.connector.connect(user=db_user,
-                                password=db_password,
-                                host=db_host,
-                                database=db_name)
+                                   password=db_password,
+                                   host=db_host,
+                                   database=db_name)
 
 
 def main():
