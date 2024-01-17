1cfdc99d1bd740902f697abd7cb5faea2090da9c
dom <6157@holbertonstudents.com>
2024-01-17 13:25:28 -0600

    fixed indentation error

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -17,8 +17,8 @@ class BasicAuth(Auth):
         pass
 
 
-def extract_base64_authorization_header(self,
-                                        authorization_header: str) -> str:
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
         """extract_base64_authorization_header method that returns base64pt"""
         if authorization_header is None:
             return None
@@ -29,9 +29,9 @@ def extract_base64_authorization_header(self,
         return authorization_header[6:]
 
 
-def decode_base64_authorization_header(self,
-                                       base64_authorization_header:
-                                       str) -> str:
+    def decode_base64_authorization_header(self,
+                                          base64_authorization_header:
+                                          str) -> str:
         """ decode base64 authorization header """
         if base64_authorization_header is None:
             return None
@@ -46,9 +46,9 @@ def decode_base64_authorization_header(self,
             return None
 
 
-def extract_user_credentials(self,
-                             decoded_base64_authorization_header: str
-                             ) -> (str, str):
+    def extract_user_credentials(self,
+                                decoded_base64_authorization_header: str
+                                ) -> (str, str):
         """ extract_user_credentials """
         if decoded_base64_authorization_header is None:
             return None, None
@@ -60,8 +60,8 @@ def extract_user_credentials(self,
         return user_credentials[0], user_credentials[1]
 
 
-def user_object_from_credentials(self, user_email: str,
-                                 user_pwd: str) -> TypeVar('User'):
+    def user_object_from_credentials(self, user_email: str,
+                                    user_pwd: str) -> TypeVar('User'):
         """user_object_from_credentials method"""
         if user_email is None or type(user_email) is not str:
             return None
@@ -77,7 +77,7 @@ def user_object_from_credentials(self, user_email: str,
         return None
 
 
-def current_user(self, request=None) -> TypeVar('User'):
+    def current_user(self, request=None) -> TypeVar('User'):
         """
         Retrieves the User instance for a request using Basic Authentication.
         """
