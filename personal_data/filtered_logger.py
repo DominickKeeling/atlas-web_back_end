339070faf754d97ad8c59c88ff81e8ac868dfa3a
dom <6157@holbertonstudents.com>
2024-01-17 10:49:41 -0600

    addjusting indentation

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -13,13 +13,13 @@ PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
 
 
 def filter_datum(fields: List[str],
-                   redaction: str,
-                   message: str,
-                   separator: str) -> str:
+                redaction: str,
+                message: str,
+                separator: str) -> str:
     """ returns log message """
     for field in fields:
         message = re.sub(f'{field}=.*?{separator}',
-                         f'{field}={redaction}{separator}', message)
+                        f'{field}={redaction}{separator}', message)
     return message
 
 
