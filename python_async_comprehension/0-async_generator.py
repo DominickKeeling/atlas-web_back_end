c2a252d78f94957e0e02e0a861650b9a5163d558
dom <6157@holbertonstudents.com>
2024-05-05 23:33:19 -0500

    initial push for tasks

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,17 +1,21 @@
 #!/usr/bin/env python3
 
-"""Write a coroutine called async_generator that takes no arguments.The
-coroutine will loop 10 times, each time asynchronously wait 1 second, then
-yield a random number between 0 and 10. Use the random module."""
+"""
+Write a coroutine called async_generator that takes no arguments.
 
+The coroutine will loop 10 times, each time asynchronously wait 1 second,
+then yield a random number between 0 and 10. Use the random module.
+"""
 
 import asyncio
 import random
-from typing import Generator
+from typing import AsyncGenerator
 
 
-async def async_generator() -> Generator[float, None, None]:
-    """Async generator yields a random number between 0 and 10"""
+async def async_generator() -> AsyncGenerator[float, None]:
+    """
+    yeilds random number between 0 and 10
+    """
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.uniform(0, 10)
+        yield random.uniform(0,10)
---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+"""
+Import wait_random from the previous python file that you’ve written and write
+an async routine called wait_n that takes in 2 int arguments (in this order): n
+and max_delay. You will spawn wait_random n times with the specified max_delay.
+
+wait_n should return the list of all the delays (float values). The list of the
+delays should be in ascending order without using sort() because of
+concurrency.
+"""
+
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+import random
+from typing import List
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Returns list of delays
+    """
+    opperations = [wait_random(max_delay) for _ in range(n)]
+    results = await asyncio.gather(*opperations)
+    return sorted(results)
