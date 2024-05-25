52f4267f9ee552c0bf226ecb340f1c5368b08104
dom <6157@holbertonstudents.com>
2024-05-24 20:35:00 -0500

    fixing logic

--- caching/2-lifo_cache.py
+++ caching/2-lifo_cache.py
@@ -39,9 +39,11 @@ class LIFOCache(BaseCaching):
         self.cache_data[key] = item
 
         if len(self.cache_data) > self.MAX_ITEMS:
+            self.cache_data[key] = item
             latest_key = self.order.pop(0)
             del self.cache_data[latest_key]
             print(f"DISCARD: {latest_key}")
+            set.put(key, item)
 
     def get(self, key):
         """
