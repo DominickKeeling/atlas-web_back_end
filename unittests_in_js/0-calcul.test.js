2ea9a50234513c896764fc61358cfff07a39e8e4
dom <6157@holbertonstudents.com>
2024-06-27 11:10:17 -0500

    added another test case in the 0-calcul.test.js file

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -11,6 +11,10 @@ describe('calculateNumber', function() {
         assert.strictEqual(calculateNumber(1, 3.7), 5);
     });
 
+    it('should return 5 when adding 1.2 and 3.7', function() {
+        assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+    });
+
     it('should return 3 when adding 1.5 and 3.7', function() {
         assert.strictEqual(calculateNumber(1.5, 3.7), 6);
     });
--- unittests_in_js/utils.js
+++ unittests_in_js/utils.js
@@ -13,12 +13,4 @@ const Utils = {
     }
 };
 
-const Utils = require('./utils');
-
-exports.sendPaymentRequestToApi = function(totalAmount, totalDiscount) {
-    const result = Utils.calculateNumber('SUM', totalAmount, totalDiscount);
-    console.log(`Sending payment request with result: ${result}`);
-};
-
-
 module.exports = Utils;
