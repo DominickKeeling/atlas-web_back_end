d3eb30ff5fd4cb5069220d31b4649661c4950554
dom <6157@holbertonstudents.com>
2024-06-27 16:01:05 -0500

    failing

--- unittests_in_js/3-payment.test.js
+++ unittests_in_js/3-payment.test.js
@@ -2,24 +2,16 @@ const chai = require('chai');
 const sinon = require('sinon');
 const expect = chai.expect;
 const Utils = require('./utils');
-const sendPaymentRequestToApi = require('./3-payment');
+const { sendPaymentRequestToApi } = require('./3-payment');
 
 describe('sendPaymentRequestToApi', () => {
-    let calculateNumberSpy;
-
-    beforeEach(() => {
-        calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
-    });
-
-    afterEach(() => {
-        calculateNumberSpy.restore();
-    });
-
     it('should call Utils.calculateNumber with SUM, 100, 20', () => {
-        const consoleSpy = sinon.spy(console, 'log');
+        let spy = sinon.spy(Utils, 'calculateNumber');
+
         sendPaymentRequestToApi(100, 20);
-        expect(calculateNumberSpy.calledOnceWith('SUM', 100, 20)).to.be.true;
-        expect(consoleSpy.calledOnceWith('The total is: 120')).to.be.true;
-        consoleSpy.restore();
+
+        sinon.assert.calledWith(spy, 'SUM', 100, 20);
+
+        spy.resolve();
     });
 });
