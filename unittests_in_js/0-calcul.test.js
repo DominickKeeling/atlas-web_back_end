a620f2e82078344a01e6610de1f7e84e394adb60
dom <6157@holbertonstudents.com>
2024-06-27 12:15:50 -0500

    added another test case in the 0-calcul.test.js file

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -3,7 +3,7 @@ const calculateNumber = require('./0-calcul')
 const { isTypedArray } = require('util/types')
 
 describe('calculateNumber', function() {
-    it('should return 4 when adding 1 and 3.1', function() {
+    it('...', function() {
         assert.strictEqual(calculateNumber(1, 3), 4);
     });
 
