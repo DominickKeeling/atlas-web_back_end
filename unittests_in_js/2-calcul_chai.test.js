1cc0b4470023f478e56387dd68985c896e15b68b
dom <6157@holbertonstudents.com>
2024-06-27 13:53:31 -0500

    fixed dependencies

--- unittests_in_js/2-calcul_chai.test.js
+++ unittests_in_js/2-calcul_chai.test.js
@@ -1,30 +1,25 @@
-const { expect } = require('chai');
+const chai = require('chai');
+const expect = chai.expect;
 const calculateNumber = require('./2-calcul_chai');
 
 describe('calculateNumber', () => {
-    describe('SUM', () => {
-        it('should return 6 when adding 1.4 and 4.5', () => {
-            expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
-        });
-
-        it('should return 0 when adding -1.4 and 1.4', () => {
-            expect(calculateNumber('SUM', -1.4, 1.4)).to.equal(0);
-        });
+    it('should return 6 when adding 1.4 and 4.5', () => {
+        expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
     });
 
-    describe('SUBTRACT', () => {
-        it('should return 3 when subtracting 4.5 from 1.4', () => {
-            expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(3);
-        });
+    it('should return 0 when adding -1.4 and 1.4', () => {
+        expect(calculateNumber('SUM', -1.4, 1.4)).to.equal(0);
     });
 
-    describe('DIVIDE', () => {
-        it('should return 0.2 when dividing 1.4 by 4.5', () => {
-            expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
-        });
+    it('should return 3 when subtracting 1.4 from 4.4', () => {
+        expect(calculateNumber('SUBTRACT', 4.4, 1.4)).to.equal(3);
+    });
 
-        it('should return Error when dividing 1.4 by 0', () => {
-            expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
-        });
+    it('should return 0.2 when dividing 1.4 by 4.5', () => {
+        expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
+    });
+
+    it('should return Error when dividing 1.4 by 0', () => {
+        expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
     });
 });
