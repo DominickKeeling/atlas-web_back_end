50d1a5c916aeff0859be80a5a3143ff288779e60
Dom <118311759+DominickKeeling@users.noreply.github.com>
2024-09-30 09:27:17 -0500

    Delete Node_JS directory

--- Node_JS/0-console.js
+++
@@ -1,7 +0,0 @@
-// displays message to the console.
-function displayMessage(message) {
-    console.log(message);
-}
-
-// Exports the displayMessage function to be used in other modules.
-module.exports = displayMessage;
