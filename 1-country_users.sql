2068e8485f69bc2c373ced7a61486ed39c82f3bb
Dom <118311759+DominickKeeling@users.noreply.github.com>
2024-09-30 09:30:32 -0500

    Delete 1-country_users.sql

--- 1-country_users.sql
+++
@@ -1,9 +0,0 @@
--- Write a SQL script that creates a table users
--- with four columns: id, email, name, county
-CREATE TABLE IF NOT EXISTS users (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255),
-    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
-);
-
