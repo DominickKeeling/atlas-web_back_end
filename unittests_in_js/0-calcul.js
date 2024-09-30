b6769cbd1bfd0cc3b88b10f7bea0d728a9f50636
Dom <118311759+DominickKeeling@users.noreply.github.com>
2024-09-30 09:25:11 -0500

    Delete unittests_in_js directory

--- unittests_in_js/0-calcul.js
+++
@@ -1,9 +0,0 @@
-//
-function calculateNumber(a, b) {
-    const roundedA = Math.round(a);
-    const roundedB = Math.round(b);
-    const sum = roundedA + roundedB;
-    return sum;
-}
-
-module.exports = calculateNumber;
