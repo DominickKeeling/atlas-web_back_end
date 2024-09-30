97498f7f0f8e9785ba0128a7bdc0ea6c755a6363
Dom <118311759+DominickKeeling@users.noreply.github.com>
2024-09-30 09:28:32 -0500

    Delete 0-uniq_users.sql

--- 0-uniq_users.sql
+++
@@ -1,8 +0,0 @@
--- Write a sql script that creates a table users
--- table contains 3 columns: id, email, name
-CREATE TABLE IF NOT EXISTS users  (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  email VARCHAR(225) NOT NULL UNIQUE,
-  name VARCHAR(225)
-);
-
